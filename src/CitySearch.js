a5e1c9f004d5959c7c39c17473ba723be246fb7a
Koola123 <codemonkey@web.de>
2021-12-13 17:50:52 +0100

    Show a list of suggestions

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react'
 class CitySearch extends Component {
   state = {
     query: "",
+    suggestions: []
   }
 
   handleInputChanged = (event) => {
-    const value = event.target.value
-    this.setState({ query: value})
-  }
+  const value = event.target.value;
+  const suggestions = this.props.locations.filter((location) => {
+    return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+  });
+  this.setState({
+    query: value,
+    suggestions,
+  });
+};
   render() {
     return(
       <div className="CitySearch">
@@ -18,6 +25,12 @@ class CitySearch extends Component {
         value={this.state.query}
         onChange={this.handleInputChanged} />
         <ul className="suggestions">
+          {this.state.suggestions.map((suggestion) => {
+            return <li key={suggestion}>{suggestion}</li>
+          })}
+          <li key="all">
+          <b>See all cities</b>
+          </li>
         </ul>
       </div>
     )
